75d95701a1147654c8e8496796385971cc24db1e
andrewandy11 <118780444+andrewandy11@users.noreply.github.com>
2022-11-22 12:10:53 +0000

    Update list.java

--- list.java
+++ list.java
@@ -6,3 +6,7 @@ legah
 andre
 glory
 godwin
+have made some changes in my code now to see if can pull them back to my local remote server
+happy learning new amazing skills as this 
+will definitely come back to testify when i start working as a snr devops engr
+date
